4d9073384d11a07b2341912753829546573f18ee
Danilo Chaves <danchavesds@gmail.com>
2025-03-07 22:47:44 -0300

    initial commit

---
+++ src/main/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.startec.documentsignerusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSignerUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSignerUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.startec.documentsignerusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSignerUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

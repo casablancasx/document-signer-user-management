8014be5ca3d1b552eaeb9d08d900e3a34eb7eaf3
Danilo Chaves <danchavesds@gmail.com>
2025-03-07 22:47:56 -0300

    initial commit

--- src/main/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplication.java
+++ src/main/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DocumentSignerUserManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocumentSignerUserManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentSignerUserManagementApplication.class, args);
+    }
 
 }
--- src/test/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplicationTests.java
+++ src/test/java/br/com/startec/documentsignerusermanagement/DocumentSignerUserManagementApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DocumentSignerUserManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

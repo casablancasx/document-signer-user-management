906c45d99183561eebd93b71bd01cbd9817f72a7
Danilo Chaves <danchavesds@gmail.com>
2025-03-10 22:36:45 -0300

    feat: Implement security filter for JWT token validation and user authentication

--- src/main/java/br/com/startec/documentsignerusermanagement/infrastructure/security/SecurityFilter.java
+++ src/main/java/br/com/startec/documentsignerusermanagement/infrastructure/security/SecurityFilter.java
@@ -1,18 +1,42 @@
 package br.com.startec.documentsignerusermanagement.infrastructure.security;
 
+import br.com.startec.documentsignerusermanagement.infrastructure.persistence.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+        if (token != null) {
+            String login =  tokenService.validateToken(token);
+            UserDetails user = userRepository.findByEmail(login);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
 
+
+    public String recoverToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null) return null;
+        return token.replace("Bearer ", "");
     }
 }
